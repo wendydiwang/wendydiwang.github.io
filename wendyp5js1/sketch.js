71be37683f42098d9f5fa302178d7253f650724a
wendydiwang <mac@MacdeMacBook-Pro.local>
2016-12-13 11:00:34 -0800

    test

---
+++ wendyp5js1/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+}
+
+function draw() {
+
+  fill(random(255), random(255), random(255),random(100));
+  noStroke();
+  ellipse(random(800), random(800), random((10, 100)));
+
+
+
+}
